c7085796356fa548314db935c0e4bd79f07e0e81
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-07-31 22:07:01 +0300

    Kept screen on while app running.

--- app/src/main/java/com/example/pomodoroapp/MainActivity.kt
+++ app/src/main/java/com/example/pomodoroapp/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.pomodoroapp
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
