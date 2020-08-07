45d55198cddb613f4ba34f30e3873d2c28bb9267
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-08-07 22:28:07 +0300

    #19 Huawei ads kit dependecy added and banner view created to the fragment timer, loaded the ads to the view.

--- app/src/main/java/com/example/pomodoroapp/MainActivity.kt
+++ app/src/main/java/com/example/pomodoroapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pomodoroapp
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.huawei.hms.ads.HwAds
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
@@ -13,5 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         Timber.plant(DebugTree())
+
+        // Initialize the HUAWEI Ads SDK.
+        HwAds.init(this)
     }
 }
