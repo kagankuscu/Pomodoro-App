5c424989b05326b1d1ad88c3c5ded55041a29e56
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-08-16 16:25:18 +0300

    Splash screen added.

---
+++ app/src/main/java/com/example/pomodoroapp/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package com.example.pomodoroapp.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pomodoroapp.MainActivity
+import com.example.pomodoroapp.R
+import com.huawei.hms.ads.HwAds
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        iv_note.alpha = 0f
+        iv_note.animate().setDuration(1500).alpha(1f).withEndAction {
+
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+
+    }
+}
