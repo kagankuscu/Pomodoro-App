bbe36a6855c97b7e87e9c93face87c007590f548
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-09-12 18:28:04 +0300

    #41 I tried how can i get the preferences value. Now i have to use it.

--- app/src/main/java/com/example/pomodoroapp/setting/SettingsFragment.kt
+++ app/src/main/java/com/example/pomodoroapp/setting/SettingsFragment.kt
@@ -3,10 +3,17 @@ package com.example.pomodoroapp.setting
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import com.example.pomodoroapp.R
+import timber.log.Timber
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        val pre = preferenceScreen.sharedPreferences.all
+
+        
+        Timber.i(pre.toString())
+        Timber.i(pre.keys.toString())
+        Timber.i(pre["work_time"].toString())
     }
 }
